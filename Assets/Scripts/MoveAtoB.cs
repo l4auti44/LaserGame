c47699facc8d7d83e9d92582607a029e25bfc715
l4auti44 <lautarolamberti.44@gmail.com>
2023-12-10 17:49:04 -0300

    added moving object behavior

---
+++ Assets/Scripts/MoveAtoB.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAtoB : MonoBehaviour
+{
+    [SerializeField] private Transform pointA;
+    [SerializeField] private Transform pointB;
+    [SerializeField] private float speed;
+    private Vector3 targetPosition;
+    // Update is called once per frame
+    private void Start()
+    {
+        targetPosition = pointA.position;
+    }
+    void Update()
+    {
+       transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, targetPosition) < 0.001f)
+        {
+            targetPosition = targetPosition == pointA.position ? pointB.position : pointA.position;
+        }
+    }
+}
