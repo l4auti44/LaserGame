12c4fb24416836588fc68d6696c865ae81f60d8a
l4auti44 <lautarolamberti.44@gmail.com>
2023-12-16 20:05:09 -0300

    fixed timer highscore

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,10 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    
+    private void Start()
+    {
+        if (PlayerPrefs.GetFloat(SceneManager.GetActiveScene().name) == 0)
+        {
+            PlayerPrefs.SetFloat(SceneManager.GetActiveScene().name, 9999f);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (GameController.Instance.timer > PlayerPrefs.GetFloat(SceneManager.GetActiveScene().name))
+        if (GameController.Instance.timer < PlayerPrefs.GetFloat(SceneManager.GetActiveScene().name))
             PlayerPrefs.SetFloat(SceneManager.GetActiveScene().name, GameController.Instance.timer);
         GameController.Instance.Win();
     }
--- Assets/Scripts/Game/LoadBestTimes.cs
+++ Assets/Scripts/Game/LoadBestTimes.cs
@@ -12,8 +12,19 @@ public class LoadBestTimes : MonoBehaviour
     private void Start()
     {
         float text1 = PlayerPrefs.GetFloat("Level 1");
-        level1.text = PlayerPrefs.GetFloat("Level 1").ToString("00:00");
-        level2.text = PlayerPrefs.GetFloat("Level 2").ToString("00:00");
-        level3.text = PlayerPrefs.GetFloat("Level 3").ToString("00:00");
+        if (PlayerPrefs.GetFloat("Level 1") != 9999f)
+        {
+            level1.text = PlayerPrefs.GetFloat("Level 1").ToString("00:00");
+        }
+        if (PlayerPrefs.GetFloat("Level 2") != 9999f)
+        {
+            level2.text = PlayerPrefs.GetFloat("Level 2").ToString("00:00");
+        }
+        if (PlayerPrefs.GetFloat("Level 3") != 9999f)
+        {
+            level3.text = PlayerPrefs.GetFloat("Level 3").ToString("00:00");
+        }
+        
+
     }
 }
